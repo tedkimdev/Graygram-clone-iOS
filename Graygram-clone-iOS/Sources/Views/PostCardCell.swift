3d0abf78231336732b0037b08521f14664cbd754
aney <yenafirst91@gmail.com>
2017-03-12 21:12:08 +0900

    좋아요 영역과 메시지 영역 위치를 바꿈

--- Graygram-clone-iOS/Sources/Views/PostCardCell.swift
+++ Graygram-clone-iOS/Sources/Views/PostCardCell.swift
@@ -25,21 +25,21 @@ class PostCardCell: UICollectionViewCell {
     
     static let photoViewTop = 10.f
     
-    static let messageLabelTop = 10.f
-    static let messageLabelLeft = 10.f
-    static let messageLabelRight = 10.f
-    
     static let likeButtonTop = 10.f
     static let likeButtonLeft = 10.f
     static let likeButtonSize = 20.f
     
     static let likeCountLabelLeft = 6.f
+    
+    static let messageLabelTop = 10.f
+    static let messageLabelLeft = 10.f
+    static let messageLabelRight = 10.f
   }
   
   fileprivate struct Font {
     static let usernameLabel = UIFont.boldSystemFont(ofSize: 13)
-    static let messageLabel = UIFont.systemFont(ofSize: 14)
     static let likeCountLabel = UIFont.boldSystemFont(ofSize: 12)
+    static let messageLabel = UIFont.systemFont(ofSize: 14)
   }
   
   
@@ -55,16 +55,16 @@ class PostCardCell: UICollectionViewCell {
   fileprivate let photoView = UIImageView().then {
     $0.backgroundColor = .lightGray
   }
-  fileprivate let messageLabel = UILabel().then {
-    $0.numberOfLines = Constant.messageLabelNumberOfLines
-    $0.font = Font.messageLabel
-  }
   fileprivate let likeButton = UIButton().then {
     $0.setBackgroundImage(#imageLiteral(resourceName: "icon-like"), for: .normal)
     $0.setBackgroundImage(#imageLiteral(resourceName: "icon-like-selected"), for: .normal)
   }
   fileprivate let likeCountLabel = UILabel().then {
     $0.font = Font.likeCountLabel
+  }
+  fileprivate let messageLabel = UILabel().then {
+    $0.numberOfLines = Constant.messageLabelNumberOfLines
+    $0.font = Font.messageLabel
   }
   
   
@@ -75,9 +75,9 @@ class PostCardCell: UICollectionViewCell {
     self.contentView.addSubview(self.userPhotoView)
     self.contentView.addSubview(self.usernameLabel)
     self.contentView.addSubview(self.photoView)
-    self.contentView.addSubview(self.messageLabel)
     self.contentView.addSubview(self.likeButton)
     self.contentView.addSubview(self.likeCountLabel)
+    self.contentView.addSubview(self.messageLabel)
   }
   
   required init?(coder aDecoder: NSCoder) {
@@ -91,14 +91,14 @@ class PostCardCell: UICollectionViewCell {
     self.userPhotoView.setImage(with: post.user.photoID, size: .tiny)
     self.usernameLabel.text = post.user.username
     self.photoView.setImage(with: post.photoID, size: .hd)
-    self.messageLabel.text = post.message
-    self.messageLabel.isHidden = post.message?.isEmpty != false
     self.likeButton.isSelected = post.isLiked
     if post.likeCount > 0 {
       self.likeCountLabel.text = "\(post.likeCount!)명이 좋아합니다."
     } else {
-    self.likeCountLabel.text = "가장 먼저 좋아요를 눌러보세요."
+      self.likeCountLabel.text = "가장 먼저 좋아요를 눌러보세요."
     }
+    self.messageLabel.text = post.message
+    self.messageLabel.isHidden = post.message?.isEmpty != false
     self.setNeedsLayout()
   }
   
@@ -112,6 +112,9 @@ class PostCardCell: UICollectionViewCell {
     
     height += width // photoView height
     
+    height += Metric.likeButtonTop
+    height += Metric.likeButtonSize
+    
     if let message = post.message, !message.isEmpty {
       let font = Font.messageLabel
       let messageLabelSize = message.size(
@@ -122,9 +125,6 @@ class PostCardCell: UICollectionViewCell {
       height += Metric.messageLabelTop
       height += messageLabelSize.height // messageLabel height
     }
-    
-    height += Metric.likeButtonTop
-    height += Metric.likeButtonSize
     
     return CGSize(width: width, height: height)
   }
@@ -151,19 +151,7 @@ class PostCardCell: UICollectionViewCell {
     self.photoView.width = self.contentView.width
     self.photoView.height = self.photoView.width
     
-    if !self.messageLabel.isHidden {
-      self.messageLabel.top = self.photoView.bottom + Metric.messageLabelTop
-      self.messageLabel.left = Metric.messageLabelLeft
-      self.messageLabel.width = self.contentView.width - Metric.messageLabelLeft - Metric.messageLabelRight
-      self.messageLabel.sizeToFit()
-    }
-    
-    if !self.messageLabel.isHidden {
-      self.likeButton.top = self.messageLabel.bottom + Metric.likeButtonTop
-    } else {
-      self.likeButton.top = self.photoView.bottom + Metric.likeButtonTop
-    }
-    
+    self.likeButton.top = self.photoView.bottom + Metric.likeButtonTop
     self.likeButton.left = Metric.likeButtonLeft
     self.likeButton.width = Metric.likeButtonSize
     self.likeButton.height = Metric.likeButtonSize
@@ -171,6 +159,13 @@ class PostCardCell: UICollectionViewCell {
     self.likeCountLabel.sizeToFit()
     self.likeCountLabel.left = self.likeButton.right + Metric.likeCountLabelLeft
     self.likeCountLabel.centerY = self.likeButton.centerY
+    
+    if !self.messageLabel.isHidden {
+      self.messageLabel.top = self.likeButton.bottom + Metric.messageLabelTop
+      self.messageLabel.left = Metric.messageLabelLeft
+      self.messageLabel.width = self.contentView.width - Metric.messageLabelLeft - Metric.messageLabelRight
+      self.messageLabel.sizeToFit()
+    }
   }
   
 }
