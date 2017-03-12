3182103ddfd42402ee693932bf5d5d82e3482be0
aney <yenafirst91@gmail.com>
2017-03-12 17:09:52 +0900

    문자열 크기 계산을 위한 extension을 만들어서 사용

---
+++ Graygram-clone-iOS/String+BoundingRect.swift
@@ -0,0 +1,30 @@
+//
+//  String+BoundingRect.swift
+//  Graygram-clone-iOS
+//
+//  Created by aney on 2017. 3. 12..
+//  Copyright © 2017년 Taedong Kim. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+  
+  func size(width: CGFloat, font: UIFont, numberOfLines: Int = 0) -> CGSize {
+    let maximumHeight = numberOfLines == 0
+      ? CGFloat.greatestFiniteMagnitude
+      : font.lineHeight * CGFloat(numberOfLines)
+    let constraintSize = CGSize(width: width - 20, height: maximumHeight * 3)
+    let options: NSStringDrawingOptions = [.usesFontLeading, .usesLineFragmentOrigin]
+    let attributes: [String: Any] = [NSFontAttributeName: font]
+    let boundingRect = self.boundingRect(
+      with: constraintSize,
+      options: options,
+      attributes: attributes,
+      context: nil
+    )
+    
+    return CGSize(width: ceil(boundingRect.width), height: ceil(boundingRect.height))
+  }
+  
+}
