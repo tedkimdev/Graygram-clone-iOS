d49237d04f5bd3a4b07ed1d26ee858c26ba7103f
aney <yenafirst91@gmail.com>
2017-03-12 21:26:07 +0900

    409 에러가 나는 경우도 성공으로 인식

--- Graygram-clone-iOS/Sources/Views/PostCardCell.swift
+++ Graygram-clone-iOS/Sources/Views/PostCardCell.swift
@@ -201,7 +201,8 @@ class PostCardCell: UICollectionViewCell {
     
     Alamofire.request(urlString, method: .post, headers: headers).responseData { response in
       switch response.result {
-      case .success:
+      case .success,
+           .failure where response.response?.statusCode != 409:
         print("post-\(postID) 좋아요 성공!")
         
       case .failure:
