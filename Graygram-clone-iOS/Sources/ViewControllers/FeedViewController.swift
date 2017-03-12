d82f7008e6ef1eb35a40e014f7bc456e7ca158b3
aney <yenafirst91@gmail.com>
2017-03-12 19:41:43 +0900

    CollectionActivityIndicatorView를 콜렉션 뷰에 등록

--- Graygram-clone-iOS/Sources/ViewControllers/FeedViewController.swift
+++ Graygram-clone-iOS/Sources/ViewControllers/FeedViewController.swift
@@ -34,7 +34,12 @@ final class FeedViewController: UIViewController {
     self.collectionView.frame = self.view.bounds
     self.collectionView.dataSource = self
     self.collectionView.delegate = self
-      
+    self.collectionView.register(
+      CollectionActivityIndicatorView.self,
+      forSupplementaryViewOfKind: UICollectionElementKindSectionFooter,
+      withReuseIdentifier: "activityIndicatorView"
+    )
+    
     self.refreshControl.addTarget(self, action: #selector(self.refreshControlDidChangeValue), for: .valueChanged)
     
     self.collectionView.addSubview(self.refreshControl)
