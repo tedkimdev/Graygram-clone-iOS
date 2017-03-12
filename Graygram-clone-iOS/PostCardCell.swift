6d9af382dcd4a0ea55a239d32d62cefd6e5fc8de
aney <yenafirst91@gmail.com>
2017-03-12 16:10:20 +0900

    PostCardCell 클래스 추가

---
+++ Graygram-clone-iOS/PostCardCell.swift
@@ -0,0 +1,21 @@
+//
+//  PostCardCell.swift
+//  Graygram-clone-iOS
+//
+//  Created by aney on 2017. 3. 12..
+//  Copyright © 2017년 Taedong Kim. All rights reserved.
+//
+
+import UIKit
+
+class PostCardCell: UICollectionViewCell {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
