9ea96785f6337f07032e527b452016dcabea6266
aney <yenafirst91@gmail.com>
2017-03-12 18:29:31 +0900

    MARK:를 사용해서 FeedViewController 코드 그룹

--- Graygram-clone-iOS/Sources/ViewControllers/FeedViewController.swift
+++ Graygram-clone-iOS/Sources/ViewControllers/FeedViewController.swift
@@ -11,13 +11,21 @@ import Alamofire
 
 final class FeedViewController: UIViewController {
   
+  // MARK: Properties
+  
   fileprivate var posts: [Post] = []
+  
+  
+  // MARK: UI
   
   fileprivate let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
     $0.backgroundColor = .white
     $0.register(PostCardCell.self, forCellWithReuseIdentifier: "cardCell")
   }
-
+  
+  
+  // MARK: View Life Cycle
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     self.collectionView.frame = self.view.bounds
@@ -26,6 +34,7 @@ final class FeedViewController: UIViewController {
     self.view.addSubview(self.collectionView)
     self.fetchPost()
   }
+  
   
   // MARK: Networking
   
