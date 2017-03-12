84f79d7b202e86d5b126d843d6693d2800109daa
aney <yenafirst91@gmail.com>
2017-03-12 15:48:01 +0900

    Post 모델 작성

---
+++ Graygram-clone-iOS/Post.swift
@@ -0,0 +1,26 @@
+//
+//  Post.swift
+//  Graygram-clone-iOS
+//
+//  Created by aney on 2017. 3. 12..
+//  Copyright © 2017년 Taedong Kim. All rights reserved.
+//
+
+import ObjectMapper
+
+struct Post: Mappable {
+  
+  var id: Int!
+  var message: String?
+  var createdAt: Date!
+  
+  init?(map: Map) {
+  }
+  
+  mutating func mapping(map: Map) {
+    self.id <- map["id"]
+    self.message <- map["message"]
+    self.createdAt <- (map["created_at"], ISO8601DateTransform())
+  }
+  
+}
