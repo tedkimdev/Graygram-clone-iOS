1b64e2e5bcd42fc32de14f8947d6f9a83ba6eed3
aney <yenafirst91@gmail.com>
2017-03-12 15:24:11 +0900

    Initial Commit & CocoaPods을 이용해서 Alamofire 설치

---
+++ Graygram-clone-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Graygram-clone-iOS
+//
+//  Created by aney on 2017. 3. 12..
+//  Copyright © 2017년 Taedong Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+target 'Graygram-clone-iOS' do
+  use_frameworks!
+
+  # Networking
+  pod 'Alamofire', '~> 4.3'
+
+end
