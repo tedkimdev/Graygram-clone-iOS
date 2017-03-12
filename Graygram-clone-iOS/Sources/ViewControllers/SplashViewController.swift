d74fdf14d89a81587404d3cff831d8f1482e41fb
aney <yenafirst91@gmail.com>
2017-03-12 20:42:17 +0900

    빈 SplashViewController 작성

---
+++ Graygram-clone-iOS/Sources/ViewControllers/SplashViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SplashViewController.swift
+//  Graygram-clone-iOS
+//
+//  Created by aney on 2017. 3. 12..
+//  Copyright © 2017년 Taedong Kim. All rights reserved.
+//
+
+import UIKit
+
+final class SplashViewController: UIViewController {
+  
+}
