33fedf3a878def980500443317c94713e25ba9bc
aney <yenafirst91@gmail.com>
2017-03-14 22:59:41 +0900

    PostEditorViewController 추가

---
+++ Graygram-clone-iOS/Sources/ViewControllers/PostEditorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PostEditorViewController.swift
+//  Graygram-clone-iOS
+//
+//  Created by aney on 2017. 3. 14..
+//  Copyright © 2017년 Taedong Kim. All rights reserved.
+//
+
+import UIKit
+
+class PostEditorViewController: UIViewController {
+  
+  // MARK: View Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
