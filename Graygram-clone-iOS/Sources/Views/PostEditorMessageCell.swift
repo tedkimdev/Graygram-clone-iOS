965c1574bcbe33fefad28cae6d40cad0509f9752
aney <yenafirst91@gmail.com>
2017-03-14 23:18:51 +0900

    PostEditorMessageCell 추가

---
+++ Graygram-clone-iOS/Sources/Views/PostEditorMessageCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostEditorMessageCell.swift
+//  Graygram-clone-iOS
+//
+//  Created by aney on 2017. 3. 14..
+//  Copyright © 2017년 Taedong Kim. All rights reserved.
+//
+
+import UIKit
+
+final class PostEditorMessageCell: UITableViewCell {
+  
+  // MARK: Initializing
+  
+  override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
